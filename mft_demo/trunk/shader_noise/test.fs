87fee47b00d716aabab0d52059cec6cf235722a0
mchouza <mchouza@0a366488-209b-11de-b56b-c9554f25afe1>
2009-05-02 20:11:49 +0000

    - Big reorganization: deleted duplicated projects.

--- mft_demo/trunk/shader_noise/test.fs
+++
@@ -1,8 +0,0 @@
-varying vec4 pos;
-void main()
-{
-    gl_FragColor = vec4(cos(8.0 * atan(pos.y, pos.x)),
-                        cos(50.0 * (length(pos.xy) * cos(8.0 * atan(pos.y, pos.x)))),
-                        cos(50.0 * pos.y),
-                        0.0);
-}
--- mft_demo/trunk/shader_test/test.vs
+++
@@ -1,6 +0,0 @@
-varying vec3 pos;
-void main()
-{
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-    pos = vec3(gl_Vertex);
-}
